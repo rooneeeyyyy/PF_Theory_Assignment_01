5f608b8ffeb40d93b224fbb0245c52616477307d
rooneeeyyyy <ruhaanh2@gmail.com>
2026-09-12 03:51:15 +0500

    Update Question_01.c

--- Question_01.c
+++ Question_01.c
@@ -1,12 +1,1 @@
-/*Question 1
-Hotel Booking System.*/
 
-#include<stdio.h>
-
-int main(){
-    int NumGuest, Total, Discount, Standard, Deluxe, Suite;
-
-    printf("Enter the number of guests: \n");
-    scanf("%d",&NumGuest);
-
-}
