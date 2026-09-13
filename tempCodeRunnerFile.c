aff763bea67f52acf29a6ead1cc5fc1278b80ada
Ruhaan_Hussain <ruhaanh2@gmail.com>
2026-09-13 16:19:15 +0500

    Added new C program

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+
+    int N, floor, currentfloor = 0;
+
+    printf("Enter the number of floor requests: ");
+    scanf("%d", &N);
+
+    for (int i = 1; i <= N; i++) {
+
+        printf("Enter floor request %d: ", i);
+        scanf("%d", &floor);
+
+        if (floor > currentfloor) {
+            printf("Moving Up\n");
+        }
+        else if (floor < currentfloor) {
+            printf("Moving Down\n");
+        }
+        else {
+            printf("Doors Opening\n");
+        }
+
+        currentfloor = floor;
+    }
+
+    return 0;
+}
